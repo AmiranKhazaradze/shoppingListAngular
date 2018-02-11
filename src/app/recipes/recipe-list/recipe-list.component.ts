3ea177bd2c0eb033a619f30dbbe279b5462fac24
AmiranKhazaradze <khazaradze0@gmail.com>
2018-02-11 23:07:29 +0400

    section3

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,9 +8,12 @@ import {Recipe} from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
 
+  private  img1Path = 'https://static01.nyt.com/images/2015/08/14/dining/14ROASTEDSALMON/14ROASTEDSALMON-articleLarge.jpg';
+  private  img2Path = 'http://s3.amazonaws.com/finecooking.s3.tauntonclud.com/app/uploads/2017/04/18180350/051SIP112-grilled-mustard-rosemary-chicken-recipe-alt-main.jpg';
+
   recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://static01.nyt.com/images/2015/08/14/dining/14ROASTEDSALMON/14ROASTEDSALMON-articleLarge.jpg')
-  ];
+    new Recipe('A Test Recipe', 'This is simply a test', this.img1Path),
+    new Recipe('A Test Recipe', 'This is simply a test', this.img2Path)];
   constructor() { }
 
   ngOnInit() {
